cc0a9a87cd0deb81196499486482ef10220377a3
Ben Gottlieb <ben@standalone.com>
2023-03-30 04:22:14 -0500

    cleaned up SpotDate

--- Sources/FireSpotter/Model/SpotDate.swift
+++ Sources/FireSpotter/Model/SpotDate.swift
@@ -8,16 +8,17 @@
 import Suite
 
 public struct SpotDate: Codable, Equatable, Hashable, Sendable {
-	var day: String
-	public var time: String?
+	enum CodingKeys: String, CodingKey { case dayString = "day", timeString = "time" }
+	var dayString: String
+	public var timeString: String?
 	
 	public var date: Date {
 		get {
-			guard let date = DateFormatter.dmyDecoder.date(from: day) else {
+			guard let date = DateFormatter.dmyDecoder.date(from: dayString) else {
 				return .now
 			}
 			
-			if let time, let timeInfo = Date.Time(string: time) {
+			if let time = timeString, let timeInfo = Date.Time(string: time) {
 				return date.bySetting(time: timeInfo)
 			}
 			
@@ -25,21 +26,35 @@ public struct SpotDate: Codable, Equatable, Hashable, Sendable {
 		}
 		
 		set {
-			day = DateFormatter.dmyDecoder.string(from: newValue.noon)
-			if time != nil {
-				time = String(format: "%02d:%02d", newValue.hour, newValue.minute)
+			dayString = DateFormatter.dmyDecoder.string(from: newValue.noon)
+			if timeString != nil {
+				timeString = String(format: "%02d:%02d", newValue.hour, newValue.minute)
 			}
 		}
 	}
 	
-	public func formatted(date dateStyle: Date.FormatStyle.DateStyle = .long, time timeStye: Date.FormatStyle.TimeStyle = .shortened) -> String {
+	public var time: Date.Time? {
+		get {
+			guard let timeString else { return nil }
+			return .init(string: timeString)
+		}
+		set {
+			if let newValue {
+				timeString = String(format: "%02d:%02d", newValue.hour, newValue.minute)
+			} else {
+				timeString = nil
+			}
+		}
+	}
+	
+	public func formatted(date dateStyle: Date.FormatStyle.DateStyle = .long, time timeStyle: Date.FormatStyle.TimeStyle = .shortened) -> String {
 		if !hasTime {
 			return dateStyle == .omitted ? "" : date.formatted(date: dateStyle, time: .omitted)
 		}
-		return date.formatted(date: dateStyle, time: timeStye)
+		return date.formatted(date: dateStyle, time: timeStyle)
 	}
 	
-	public var hasTime: Bool { time != nil }
+	public var hasTime: Bool { timeString != nil }
 	public var noon: SpotDate {
 		SpotDate(date.noon)
 	}
@@ -49,11 +64,11 @@ public struct SpotDate: Codable, Equatable, Hashable, Sendable {
 	}
 	
 	public init(_ date: Date, includingTime: Bool = true) {
-		day = DateFormatter.dmyDecoder.string(from: date.noon)
+		dayString = DateFormatter.dmyDecoder.string(from: date.noon)
 		if includingTime {
-			time = date.time.stringValue
+			timeString = date.time.stringValue
 		} else {
-			time = nil
+			timeString = nil
 		}
 	}
 	
